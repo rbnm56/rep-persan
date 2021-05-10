9474059820c87421405e08adafb72231ce688a50
Lore <lore_aqua6@live.com.mx>
2021-05-10 14:42:46 -0500

    Se agregan campos nuevos a la tabla clientes

--- persanv1.sql
+++ persanv1.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 10-05-2021 a las 20:13:13
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.2.33
+-- Host: localhost:3306
+-- Generation Time: May 10, 2021 at 07:41 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `persanv1`
+-- Database: `persanv1`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `adicionales`
+-- Table structure for table `adicionales`
 --
 
 CREATE TABLE `adicionales` (
@@ -37,23 +38,36 @@ CREATE TABLE `adicionales` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cliente`
+-- Table structure for table `cliente`
 --
 
 CREATE TABLE `cliente` (
   `cliente_id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `correo_cliente` varchar(30) DEFAULT NULL,
   `nombre_cliente` varchar(30) NOT NULL,
   `apellido_cliente` varchar(30) NOT NULL,
+  `telefono` bigint(11) NOT NULL,
   `direccion_cliente` text NOT NULL,
   `es_preferencial` tinyint(1) NOT NULL,
   `es_mayorista` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `cliente`
+--
+
+INSERT INTO `cliente` (`cliente_id`, `id_usuario`, `correo_cliente`, `nombre_cliente`, `apellido_cliente`, `telefono`, `direccion_cliente`, `es_preferencial`, `es_mayorista`) VALUES
+(1, 30, '', 'Lorena', 'Santamaria Ramirez', 2222222222, 'no tiene', 0, 0),
+(2, 30, '', 'Cliente 3', 'Prueba', 2222222222, '2 suer', 0, 0),
+(3, 30, '', 'Cliente 4', 'Prueba', 2222222222, '2 suer', 0, 0),
+(4, 30, '', 'Cliente 5', 'Prueba', 2222222222, '2 suer', 0, 0),
+(5, 30, '', 'Cliente 6', 'Nuevo', 2222222222, 'no', 0, 0);
+
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `inventario_materiales`
+-- Table structure for table `inventario_materiales`
 --
 
 CREATE TABLE `inventario_materiales` (
@@ -65,7 +79,7 @@ CREATE TABLE `inventario_materiales` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `inventario_productos`
+-- Table structure for table `inventario_productos`
 --
 
 CREATE TABLE `inventario_productos` (
@@ -77,7 +91,7 @@ CREATE TABLE `inventario_productos` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `materiales`
+-- Table structure for table `materiales`
 --
 
 CREATE TABLE `materiales` (
@@ -90,7 +104,7 @@ CREATE TABLE `materiales` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pagos`
+-- Table structure for table `pagos`
 --
 
 CREATE TABLE `pagos` (
@@ -103,7 +117,7 @@ CREATE TABLE `pagos` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `permisos`
+-- Table structure for table `permisos`
 --
 
 CREATE TABLE `permisos` (
@@ -113,7 +127,7 @@ CREATE TABLE `permisos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `permisos`
+-- Dumping data for table `permisos`
 --
 
 INSERT INTO `permisos` (`permiso_id`, `nombre_permiso`, `descripción_permiso`) VALUES
@@ -122,7 +136,7 @@ INSERT INTO `permisos` (`permiso_id`, `nombre_permiso`, `descripción_permiso`) 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Table structure for table `productos`
 --
 
 CREATE TABLE `productos` (
@@ -137,7 +151,7 @@ CREATE TABLE `productos` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos_materiales`
+-- Table structure for table `productos_materiales`
 --
 
 CREATE TABLE `productos_materiales` (
@@ -149,7 +163,7 @@ CREATE TABLE `productos_materiales` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos_ventas`
+-- Table structure for table `productos_ventas`
 --
 
 CREATE TABLE `productos_ventas` (
@@ -162,20 +176,20 @@ CREATE TABLE `productos_ventas` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `proveedores`
+-- Table structure for table `proveedores`
 --
 
 CREATE TABLE `proveedores` (
   `proveedor_id` int(11) NOT NULL,
   `nombre_proveedor` varchar(30) NOT NULL,
-  `direccion_proveedor` text DEFAULT NULL,
-  `descripcion_proveedor` text DEFAULT NULL
+  `direccion_proveedor` text,
+  `descripcion_proveedor` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sucursales`
+-- Table structure for table `sucursales`
 --
 
 CREATE TABLE `sucursales` (
@@ -185,7 +199,7 @@ CREATE TABLE `sucursales` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `sucursales`
+-- Dumping data for table `sucursales`
 --
 
 INSERT INTO `sucursales` (`sucursal_id`, `nombre_sucursal`, `direccion_sucursal`) VALUES
@@ -194,7 +208,7 @@ INSERT INTO `sucursales` (`sucursal_id`, `nombre_sucursal`, `direccion_sucursal`
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `unidades`
+-- Table structure for table `unidades`
 --
 
 CREATE TABLE `unidades` (
@@ -206,7 +220,7 @@ CREATE TABLE `unidades` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -216,30 +230,29 @@ CREATE TABLE `usuarios` (
   `nombre_usuario` varchar(50) NOT NULL,
   `apellido_usuario` varchar(50) NOT NULL,
   `telefono` bigint(11) NOT NULL,
-  `direccion` text DEFAULT NULL,
+  `direccion` text,
   `id_sucursal` int(11) DEFAULT NULL,
   `id_permiso` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
 
 INSERT INTO `usuarios` (`usuario_id`, `username`, `password`, `nombre_usuario`, `apellido_usuario`, `telefono`, `direccion`, `id_sucursal`, `id_permiso`) VALUES
 (1, 'ruben.mendoza', '$2y$12$ZdY8Qr58UlbaBHJsWZ5Q6Onp3nMi/XNbUDtO0874uDOpqpf7Su66G', 'Alejandro', 'Mendoza', 2211532584, 'Nogales 15509', 1, 1),
 (17, 'admin', '$2y$12$jRYSuwAFCesUYuP7Wb5g4eItCxTKKwOeKodUA6YGfNLhcFZsL/cE6', 'Ruben Alejandro', 'Mendoza', 2211532584, 'Nogales 15509', NULL, NULL),
 (18, 'admin2', '$2y$12$Hw7BIf9TPjhQwGwpiXexi.Gah/qlI2LF.4eHw1mxtKUoMOlf2Y3Hu', 'Alejandro', 'Dominguez', 2222889988, 'Centro', NULL, NULL),
-(30, 'lore.santamaria', '$2y$12$zNHblkIBDH8ehRsHjEkW7.M7od/tB1iMRC9MO2FNZGJBCVkDVwkom', 'Lorena', 'Santamaria', 555555555, '38 sur', NULL, NULL),
+(30, 'lore.santamaria', '$2y$12$ie756w/KBuzXp1N8dSeGJuXnsVmLb8H9eJqc3mHa1WWkXji8f0udu', 'Lorena', 'Santamaria', 555555555, '38 sur', NULL, NULL),
 (33, 'sara.dominguez', '$2y$12$HfXyM9cqaNorQZFuzq9.sOjgv2x9bVK.wnk6O0N7995O/UnDw095S', 'Sara', 'Dominguez', 2221938919, 'Centro', NULL, NULL),
 (83, 'usuario.prueba7', '$2y$12$J.znGmCVZhZR7PSJDnRnbuluen4V1AA6soHzXI/T.7hmQpmRqiOF.', 'Usuario', 'Prueba', 2233333887, 'San ramon', 1, 1),
 (101, 'usuario.test', '$2y$12$zp7bUHRXGRlqwU85A/WRP.wE8hbX9gUDKIuVo7OMbgBMgGucA.OAO', 'Prueba', 'Prueba', 66666666, 'Feliciano', 1, 1),
-(111, 'roger.huesca', '$2y$12$4tLSs9amYn4PIVi/HFxcnur11TeB2KIEuWXNbEgNp43xcbmH2iPKC', 'Roger', 'Huesca', 22222222, 'uuuuuuuu', 1, 1),
 (118, 'ruben.mend', '$2y$12$X0o/FXiNkWm2JNwj69Y2rus.xGDp4FRGoNvRu8poGjDG5cFujK7aS', 'Ruben', 'Mendoza', 222222222, 'Nogales', 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ventas`
+-- Table structure for table `ventas`
 --
 
 CREATE TABLE `ventas` (
@@ -252,57 +265,58 @@ CREATE TABLE `ventas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `adicionales`
+-- Indexes for table `adicionales`
 --
 ALTER TABLE `adicionales`
   ADD PRIMARY KEY (`adicional_id`);
 
 --
--- Indices de la tabla `cliente`
+-- Indexes for table `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`cliente_id`);
+  ADD PRIMARY KEY (`cliente_id`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indices de la tabla `inventario_materiales`
+-- Indexes for table `inventario_materiales`
 --
 ALTER TABLE `inventario_materiales`
   ADD PRIMARY KEY (`inventario_material`),
   ADD KEY `id_material` (`id_material`);
 
 --
--- Indices de la tabla `inventario_productos`
+-- Indexes for table `inventario_productos`
 --
 ALTER TABLE `inventario_productos`
   ADD PRIMARY KEY (`inventario_producto_id`),
   ADD KEY `id_producto` (`id_producto`);
 
 --
--- Indices de la tabla `materiales`
+-- Indexes for table `materiales`
 --
 ALTER TABLE `materiales`
   ADD PRIMARY KEY (`material_id`),
   ADD KEY `id_proveedor` (`id_proveedor`);
 
 --
--- Indices de la tabla `pagos`
+-- Indexes for table `pagos`
 --
 ALTER TABLE `pagos`
   ADD PRIMARY KEY (`pago_id`),
   ADD KEY `id_venta` (`id_venta`);
 
 --
--- Indices de la tabla `permisos`
+-- Indexes for table `permisos`
 --
 ALTER TABLE `permisos`
   ADD PRIMARY KEY (`permiso_id`);
 
 --
--- Indices de la tabla `productos`
+-- Indexes for table `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`producto_id`),
@@ -310,7 +324,7 @@ ALTER TABLE `productos`
   ADD KEY `id_unidad` (`id_unidad`);
 
 --
--- Indices de la tabla `productos_materiales`
+-- Indexes for table `productos_materiales`
 --
 ALTER TABLE `productos_materiales`
   ADD PRIMARY KEY (`producto_materiales_id`),
@@ -318,7 +332,7 @@ ALTER TABLE `productos_materiales`
   ADD KEY `id_producto` (`id_producto`);
 
 --
--- Indices de la tabla `productos_ventas`
+-- Indexes for table `productos_ventas`
 --
 ALTER TABLE `productos_ventas`
   ADD PRIMARY KEY (`productos_venta_id`),
@@ -326,25 +340,25 @@ ALTER TABLE `productos_ventas`
   ADD KEY `id_venta` (`id_venta`);
 
 --
--- Indices de la tabla `proveedores`
+-- Indexes for table `proveedores`
 --
 ALTER TABLE `proveedores`
   ADD PRIMARY KEY (`proveedor_id`);
 
 --
--- Indices de la tabla `sucursales`
+-- Indexes for table `sucursales`
 --
 ALTER TABLE `sucursales`
   ADD PRIMARY KEY (`sucursal_id`);
 
 --
--- Indices de la tabla `unidades`
+-- Indexes for table `unidades`
 --
 ALTER TABLE `unidades`
   ADD PRIMARY KEY (`unidad_id`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`usuario_id`),
@@ -353,7 +367,7 @@ ALTER TABLE `usuarios`
   ADD KEY `id_permiso` (`id_permiso`);
 
 --
--- Indices de la tabla `ventas`
+-- Indexes for table `ventas`
 --
 ALTER TABLE `ventas`
   ADD PRIMARY KEY (`venta_id`),
@@ -362,138 +376,138 @@ ALTER TABLE `ventas`
   ADD KEY `id_sucursal` (`id_sucursal`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `adicionales`
+-- AUTO_INCREMENT for table `adicionales`
 --
 ALTER TABLE `adicionales`
   MODIFY `adicional_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `cliente`
+-- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de la tabla `inventario_materiales`
+-- AUTO_INCREMENT for table `inventario_materiales`
 --
 ALTER TABLE `inventario_materiales`
   MODIFY `inventario_material` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `inventario_productos`
+-- AUTO_INCREMENT for table `inventario_productos`
 --
 ALTER TABLE `inventario_productos`
   MODIFY `inventario_producto_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `materiales`
+-- AUTO_INCREMENT for table `materiales`
 --
 ALTER TABLE `materiales`
   MODIFY `material_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `pagos`
+-- AUTO_INCREMENT for table `pagos`
 --
 ALTER TABLE `pagos`
   MODIFY `pago_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `productos`
+-- AUTO_INCREMENT for table `productos`
 --
 ALTER TABLE `productos`
   MODIFY `producto_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `productos_materiales`
+-- AUTO_INCREMENT for table `productos_materiales`
 --
 ALTER TABLE `productos_materiales`
   MODIFY `producto_materiales_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `productos_ventas`
+-- AUTO_INCREMENT for table `productos_ventas`
 --
 ALTER TABLE `productos_ventas`
   MODIFY `productos_venta_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `proveedores`
+-- AUTO_INCREMENT for table `proveedores`
 --
 ALTER TABLE `proveedores`
   MODIFY `proveedor_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `unidades`
+-- AUTO_INCREMENT for table `unidades`
 --
 ALTER TABLE `unidades`
   MODIFY `unidad_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 
 --
--- AUTO_INCREMENT de la tabla `ventas`
+-- AUTO_INCREMENT for table `ventas`
 --
 ALTER TABLE `ventas`
   MODIFY `venta_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `inventario_materiales`
+-- Constraints for table `inventario_materiales`
 --
 ALTER TABLE `inventario_materiales`
   ADD CONSTRAINT `inventario_materiales_ibfk_1` FOREIGN KEY (`id_material`) REFERENCES `materiales` (`material_id`);
 
 --
--- Filtros para la tabla `inventario_productos`
+-- Constraints for table `inventario_productos`
 --
 ALTER TABLE `inventario_productos`
   ADD CONSTRAINT `inventario_productos_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`producto_id`);
 
 --
--- Filtros para la tabla `materiales`
+-- Constraints for table `materiales`
 --
 ALTER TABLE `materiales`
   ADD CONSTRAINT `materiales_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`proveedor_id`);
 
 --
--- Filtros para la tabla `pagos`
+-- Constraints for table `pagos`
 --
 ALTER TABLE `pagos`
   ADD CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`venta_id`);
 
 --
--- Filtros para la tabla `productos`
+-- Constraints for table `productos`
 --
 ALTER TABLE `productos`
   ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`proveedor_id`),
   ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`unidad_id`);
 
 --
--- Filtros para la tabla `productos_ventas`
+-- Constraints for table `productos_ventas`
 --
 ALTER TABLE `productos_ventas`
   ADD CONSTRAINT `productos_ventas_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`producto_id`),
   ADD CONSTRAINT `productos_ventas_ibfk_2` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`venta_id`);
 
 --
--- Filtros para la tabla `usuarios`
+-- Constraints for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`sucursal_id`),
   ADD CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`id_permiso`) REFERENCES `permisos` (`permiso_id`);
 
 --
--- Filtros para la tabla `ventas`
+-- Constraints for table `ventas`
 --
 ALTER TABLE `ventas`
   ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`cliente_id`),
